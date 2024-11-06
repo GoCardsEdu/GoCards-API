efdbbfff2ea7c31c5629f8f4726f8db9dfcf96ea
Grzegorz Ziemski <ziemski@gmail.com>
2024-11-06 19:56:07 +0100

    https://start.spring.io/

---
+++ src/main/java/pl/gocards/api/GoCardsApiApplication.java
@@ -0,0 +1,13 @@
+package pl.gocards.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoCardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoCardsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gocards/api/GoCardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gocards.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoCardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
